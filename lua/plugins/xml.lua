a86141debfeb65f76ab47f99562418e875e5a6f4
Enno Richter <enno@nerdworks.de>
2023-10-11 14:52:35 +0200

    configure plugins

---
+++ lua/plugins/xml.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "xml" })
+      end
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      table.insert(opts.sources, nls.builtins.formatting.xmllint)
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        xml = { "xmllint" },
+      },
+      formatters = {
+        xmllint = {
+          command = "xmllint",
+          args = { "--format" },
+        },
+      },
+    },
+  },
+}
