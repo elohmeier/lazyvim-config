8959bfa40272aa97e3d3ec12754bd2f103029036
Enno Richter <enno@nerdworks.de>
2023-10-16 11:51:59 +0200

    svelte lsp fix

--- lua/plugins/svelte.lua
+++ lua/plugins/svelte.lua
@@ -9,10 +9,10 @@ return {
   },
   {
     "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "svelte-language-server",
-      },
-    },
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "svelte-language-server" })
+      end
+    end,
   },
 }
