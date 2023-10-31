8f3513de3de8734d0621dfb4d60afc4e61c1ced4
Enno Richter <enno@nerdworks.de>
2023-10-31 05:08:04 +0000

    xmllint cfg

--- lua/plugins/xml.lua
+++ lua/plugins/xml.lua
@@ -25,7 +25,7 @@ return {
       formatters = {
         xmllint = {
           command = "xmllint",
-          args = { "--format" },
+          args = { "--format", "-" },
         },
       },
     },
