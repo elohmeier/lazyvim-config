ce6492676205e5c57b31b036d966b7e9d138cdb2
Enno Richter <enno@nerdworks.de>
2023-11-07 10:26:18 +0100

    php cfg

---
+++ lua/plugins/php.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "php" })
+      end
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "phpactor" })
+      end
+    end,
+  },
+}
